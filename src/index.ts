155ab9bb97b74bf25e0abd55d8f8b216c780288c
Samir Husain <samirhusain.iinvsys@gmail.com>
2026-04-02 18:41:20 +0530

    port update

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ app.onError((err: Error, c) => {
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: Number(process.env.PORT) || 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
